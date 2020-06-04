690f39df9afffb99f8382ea1ad79ebc410dd1b2e
Chgabri2 <52176942+Chgabri2@users.noreply.github.com>
2020-06-04 08:38:20 +0300

    main2.py

--- main.py
+++ main.py
@@ -31,10 +31,38 @@ def handle_user_request(user_url):
         print('Your new shortened url: %s ' % (url_dict[user_url]))
 
 
-def main():
+def get_new_url():
     user_url = input("Enter your url: ")
     handle_user_request(user_url)
     print(url_dict)
+    
+ 
+def go_to_url():
+    adress = input("enter the url:\n")
+    if not adress.startswith('"our_domain/'):
+        print("error! url not legal")
+    adress = adress[len("our_domain/ "):-1]
+    for key in all_data_dict.keys():
+        if adress == all_data_dict[key]:
+            my_url = key
+            webbrowser.open(my_url)
+            return
+    print("error! unknown adress")
+
+
+def main() :
+    while 1:
+        choice=input("choose 1 get a new url \nchoose 2 to use your new url \nchoose 3 to exit\n")
+        if int(choice)==1 :
+            get_new_url()
+        elif int(choice)==2:
+            go_to_url()
+            break
+        elif int(choice)==0:
+            break
+        else:
+            print("error! please enter 1,2 or 3")
+            continue
 
 
 if __name__ == '__main__':

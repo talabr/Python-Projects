e32c382779a84e38d79ea7026fdc0ad240077afc
Chgabri2 <52176942+Chgabri2@users.noreply.github.com>
2020-06-11 19:02:52 +0300

    Update my_flask_file.py

--- Shortened URL/my_flask_file.py
+++ Shortened URL/my_flask_file.py
@@ -13,9 +13,15 @@ def home():
 
 @app.route('/', methods=['POST'])
 def change():
-    origin_url = request.form['url']
-    shorten_url = urljoin(request.url_root, url_for('convert', url=get_shorten_url(origin_url)))
-    return render_template("home.html", origin_url=origin_url, message=shorten_url)
+	origin_url = request.form['url']
+	check = request.form["check_costum"]
+	if check == 'on':
+		url = request.form['costum_url']
+		new_costum_url = urljoin(request.url_root, url_for('convert', url=costum_url(origin_url, url)))
+		return render_template("home.html", origin_url=origin_url, message=new_costum_url)
+	shorten_url = urljoin(request.url_root, url_for('convert', url=get_shorten_url(origin_url)))
+	return render_template("home.html", origin_url=origin_url, message=shorten_url)
+
 
 
 @app.route('/convert/<url>')

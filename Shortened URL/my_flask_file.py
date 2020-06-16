b7beb57a951feac3a76ce17a86382939c28a4d80
talabr <talabr@hotmail.com>
2020-06-17 00:02:00 +0300

    improved functions and fix css

--- Shortened URL/my_flask_file.py
+++ Shortened URL/my_flask_file.py
@@ -1,6 +1,6 @@
 from urllib.parse import urljoin
 from flask import Flask, render_template, request, redirect, url_for
-from url_shortened_utils import create_db, get_shorten_url, get_origin_url, custom_url
+from url_shortened_utils import create_db, get_shorten_url, get_origin_url_from_shorten_url
 
 app = Flask(__name__)
 create_db()
@@ -14,40 +14,17 @@ def home():
 @app.route('/', methods=['POST'])
 def change():
     origin_url = request.form['url']
-    print("received original url %s" % origin_url)
-    # try:
-    if request.form.get('check_custom'):
-        # request.form["check_custom"]  # checks whether the checkbox is empty or not
-        user_url = request.form['custom_url']
-        user_url_after_check = custom_url(origin_url, user_url)  # returns error string or random string
-        if user_url_after_check.startswith('ERROR '):
-            return render_template("home.html", origin_url=origin_url, message=user_url_after_check)
-        else:
-            full_custom_url = urljoin(request.url_root, url_for('convert', url=user_url_after_check))
-            return render_template("home.html", origin_url=origin_url, message=full_custom_url)
-    # except:
-    else:
-        shorten_url_after_check = get_shorten_url(origin_url)  # returns error string or random string
-        if shorten_url_after_check.startswith('ERROR '):
-            return render_template("home.html", origin_url=origin_url, message=shorten_url_after_check)
-        else:
-            full_shorten_url = urljoin(request.url_root, url_for('convert', url=shorten_url_after_check))
-            return render_template("home.html", origin_url=origin_url, message=full_shorten_url)
-
-
-# def check_input(origin_url, user_url=None, is_custome=False):
-# 	if is_custome:
-# 		url = custom_url(origin_url, user_url)
-# 	else:
-# 		url = get_shorten_url(origin_url)
-# 	if url.startswith('ERROR '):
-# 		return url
-# 	return urljoin(request.url_root, url_for('convert', url))
+    custom_url = request.form.get('custom_url')
+    shorten_url_after_check = get_shorten_url(origin_url, custom_url)  # returns error string or random string
+    if shorten_url_after_check.startswith('ERROR '):
+        return render_template("home.html", origin_url=origin_url, message=shorten_url_after_check)
+    full_shorten_url = urljoin(request.url_root, url_for('convert', url=shorten_url_after_check))
+    return render_template("home.html", origin_url=origin_url, message=full_shorten_url)
 
 
 @app.route('/convert/<url>')
 def convert(url):
-    origin_url = get_origin_url(url)
+    origin_url = get_origin_url_from_shorten_url(url)
     return redirect(origin_url)
 
 
--- Shortened URL/url_shortened_utils.py
+++ Shortened URL/url_shortened_utils.py
@@ -2,9 +2,6 @@ import os
 import json
 import random
 import string
-from urllib.request import urlopen
-from urllib.error import URLError, HTTPError
-
 
 ALL_DATA_PATH = 'all_data.txt'
 
@@ -16,36 +13,6 @@ def create_db():
     """
     if not os.path.exists(ALL_DATA_PATH):
         return_data_to_file({})
-
-
-def custom_url(user_url, user_custom_url):
-    """
-    This function receives an original url and custom url and return an Error if needed
-    :param user_url:
-    :param user_custom_url:
-    :return:
-    """
-    if short_url_in_db(user_custom_url):
-        if get_key_from_value(user_custom_url) == user_url:
-            return user_custom_url
-        return 'ERROR - custom url already exist'
-    elif not validate_web_url(user_url):
-        return 'ERROR - url is not valid. Check yourself'
-    add_new_url_to_dict(user_url, user_custom_url)
-    return user_custom_url
-
-
-def url_length():
-    """
-    This function is responsible for managing the url length we randomize
-    :return: string stands for the shortened url length
-    """
-    shortened_url_length = 3
-    num = len(import_data_to_dict())//26**shortened_url_length
-    while num:
-        shortened_url_length += 1
-        num = num//26
-    return shortened_url_length
 
 
 def import_data_to_dict():
@@ -68,84 +35,6 @@ def return_data_to_file(data_dict):
         json.dump(data_dict, my_file)
 
 
-def url_exist_in_db(user_url):
-    """
-    This function checks if the url already exist in out db
-    :param user_url: the given url from the user
-    :return: boolean param. False if it does not exist and True if it does
-    """
-    return user_url in import_data_to_dict().keys()
-
-
-def short_url_in_db(user_custom_url):
-    """
-
-    :param user_custom_url:
-    :return:
-    """
-    return user_custom_url in import_data_to_dict().values()
-
-
-def get_key_from_value(custome_url):
-    my_dict = import_data_to_dict()
-    for key,value in my_dict.items():
-        if custome_url == value:
-            return key
-
-
-def validate_web_url(url):
-    """
-    This function validate the given url by trying to open the url and catching possible errors
-    :param url: user url
-    :return: error index
-    """
-    if not (url.startswith('http://') or url.startswith('https://')):
-        url = 'http://%s' % url
-    try:
-        urlopen(url)
-        return True
-    except ValueError:
-        return False
-    except HTTPError:
-        return False
-    except URLError:
-        return False
-
-
-def get_shorten_url_from_dict(user_url):
-    """
-    This function receives url and return the shortened url we created
-    :param user_url: the user url
-    :return: string of shorten url
-    """
-    return import_data_to_dict()[user_url]
-
-
-def get_origin_url(shorten_url):
-    """
-    This function receives a shorten url and return the original url that was given from the user
-    :param shorten_url: shorten url
-    :return: the original url
-    """
-    for key, value in import_data_to_dict().items():
-        if shorten_url == value:
-            return key
-
-
-def create_shortened_url(user_url):
-    """
-    This function receives the user url and randomize a new shorten url with the relevant length
-    :param user_url: the user url
-    :return: new shorten url
-    """
-    letters = string.ascii_lowercase
-    shorten_url = ''.join(random.choice(letters) for i in range(url_length()))
-    if shorten_url in import_data_to_dict().values():
-        return create_shortened_url(user_url)
-    add_new_url_to_dict(user_url, shorten_url)
-    return shorten_url
-
-
 def add_new_url_to_dict(user_url, new_shortened_url):
     """
     This function receives the user url and a shorten url and adds it to the dict and dumps to the txt file
@@ -158,15 +47,113 @@ def add_new_url_to_dict(user_url, new_shortened_url):
     return_data_to_file(all_data_dict)
 
 
-def get_shorten_url(user_url):
+def is_origin_url_exist_in_db(user_url):
+    """
+    This function checks if the url already exist in out db
+    :param user_url: the given url from the user
+    :return: boolean param. False if it does not exist and True if it does
+    """
+    return user_url in import_data_to_dict().keys()
+
+
+def is_shorten_url_exist_in_db(short_url):
+    """
+
+    :param short_url:
+    :return:
+    """
+    return short_url in import_data_to_dict().values()
+
+
+def get_shorten_url_from_origin_url(origin_url):
+    """
+    This function receives url and return the shortened url we created
+    :param origin_url: the user url
+    :return: string of shorten url
+    """
+    return import_data_to_dict()[origin_url]
+
+
+def get_origin_url_from_shorten_url(shorten_url):
+    """
+    This function receives a shorten url and return the original url that was given from the user
+    :param shorten_url: shorten url
+    :return: the original url
+    """
+    for key, value in import_data_to_dict().items():
+        if shorten_url == value:
+            return key
+
+
+def get_random_url_length():
+    """
+    This function is responsible for managing the url length we randomize
+    :return: string stands for the shortened url length
+    """
+    shortened_url_length = 3
+    num = len(import_data_to_dict()) // 26 ** shortened_url_length
+    while num:
+        shortened_url_length += 1
+        num = num // 26
+    return shortened_url_length
+
+
+def validate_web_url(url):
+    """
+    This function validate the given url by trying to open the url and catching possible errors
+    :param url: user url
+    :return: error index
+    """
+    return url.startswith('http://') or url.startswith('https://') or url.startswith('www.')
+
+
+def create_random_word():
+    letters = string.ascii_lowercase
+    random_word = ''.join(random.choice(letters) for i in range(get_random_url_length()))
+    return random_word
+
+
+def create_random_shorten_url(origin_url):
+    """
+    This function receives the user url and randomize a new shorten url with the relevant length
+    :param origin_url: the user url
+    :return: new shorten url
+    """
+    random_shorten_url = create_random_word()
+    while is_shorten_url_exist_in_db(random_shorten_url):
+        random_shorten_url = create_random_word()
+    add_new_url_to_dict(origin_url, random_shorten_url)
+    return random_shorten_url
+
+
+def get_random_shorten_url(origin_url):
     """
     This function receives the user url and returns the shorten url, whether if it exists already or not
-    :param user_url: the user url
+    :param origin_url: the user url
     :return: the shorten url
     """
-    if url_exist_in_db(user_url):
-        return get_shorten_url_from_dict(user_url)
-    elif not validate_web_url(user_url):
-        return 'ERROR - url is not valid. Check yourself'
-    return create_shortened_url(user_url)
+    if is_origin_url_exist_in_db(origin_url):
+        return get_shorten_url_from_origin_url(origin_url)
+    return create_random_shorten_url(origin_url)
 
+
+def save_custom_shorten_url(origin_url, custom_url):
+    """
+    This function receives an original url and custom url and return an Error if needed
+    :param origin_url:
+    :param custom_url:
+    :return:
+    """
+    if not is_shorten_url_exist_in_db(custom_url):
+        add_new_url_to_dict(origin_url, custom_url)
+    elif not get_origin_url_from_shorten_url(custom_url) == origin_url:
+        return 'ERROR - custom url is taken'
+    return custom_url
+
+
+def get_shorten_url(origin_url, custom_url=None):
+    if not validate_web_url(origin_url):
+        return 'ERROR - url is not valid. Check yourself'
+    if custom_url:
+        return save_custom_shorten_url(origin_url, custom_url)  # returns error string or random string
+    return get_random_shorten_url(origin_url)
